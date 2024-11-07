661fe80d86751840d5b43ef12378f8654a083181
ddeveloper72 <duncan.niamh@gmail.com>
2024-11-07 12:38:43 +0000

    Refactor app.py to enhance patient summary endpoints and add FHIR patient list functionality

--- app.py
+++ app.py
@@ -1,68 +1,105 @@
+import os
 import secrets
 import json
-from flask import Flask, jsonify
-from fhir.resources.bundle import Bundle
-from fhir.resources.patient import Patient
-from jinja2 import Environment, FileSystemLoader
-
-# Import the function that creates the sample patient record
-from create_patient_record import create_sample_patient_record
 from flask import render_template
+from flask import Flask, jsonify, redirect, request, send_from_directory, url_for
+from fhirpy import SyncFHIRClient
+from create_patient_record import create_sample_patient_record
 from convert_fhir_to_cda import fhir_to_cda
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = secrets.token_hex(16)
+app.config["SECRET_KEY"] = secrets.token_hex(16)
+
 
 # welcome page
 @app.route("/", methods=["GET"])
-def index():  
+def index():
+    """Render the index page"""
+
     return render_template("index.html")
 
 
-
-@app.route("/fhir/patient_summary", methods=["GET"])
+@app.route("/hl7/patient_summary/fhir/json", methods=["GET"])
 def get_patient_summary():
-    # Create the sample patient record as a FHIR bundle
-    patient_record = create_sample_patient_record()
+    """Generate a sample patient record and return it as a JSON object"""
 
-    # Convert FHIR resources to JSON
+    patient_record = create_sample_patient_record()
     patient_record_json = patient_record.json(indent=4)
 
     return jsonify(json.loads(patient_record_json))
 
-# # HL7 Summary endpoint (placeholder)
-# @app.route("/hl7/patient_summary", methods=["GET"])
-# def get_hl7_patient_summary():
-#     # generate a CDA document from the FHIR patient record
-#     return "HL7 Patient Summary"
-#     # Assuming you have a function to convert FHIR to CDA
 
-# HL7 Summary endpoint (placeholder)
+# @app.route("/generate_cda", methods=["POST"])
+# def generate_cda():
+#     '''Generate a CDA document from the FHIR patient record and return it as a file'''
 
-@app.route("/hl7/patient_summary", methods=["GET"])
-def get_hl7_patient_summary():
-    # Create the sample patient record as a FHIR bundle
-    patient_record = create_sample_patient_record()
+#     patient_id = request.form.get("patient_id")
 
-    # Convert FHIR resources to JSON
-    patient_record_json = patient_record.json(indent=4)
+#     if not patient_id:
+#         return redirect(url_for("index"))
+#     if patient_id:
+#         patient_record = create_sample_patient_record()
+#         fhir_to_cda(fhir_data=patient_record, patient_identifier=patient_id)
 
-    return jsonify(json.loads(patient_record_json))
+#         return redirect(url_for("download_cda", patient_id=patient_id))
 
-@app.route("/hl7/patient_summary/cda", methods=["GET"])
-def get_hl7_patient_summary_html():
-    # Create the sample patient record as a FHIR bundle
-    cda_root = fhir_to_cda(create_sample_patient_record())
-    return render_template("cda_template.html", cda_root=cda_root)
+#     return redirect(url_for("index"))
 
-    
-@app.route("/fhir/patient_summary/html", methods=["GET"])
-def get_patient_summary_html():
-    # render the FHIR patient record as HTML
-    patient_record = fhir_to_cda(create_sample_patient_record())
-    return render_template("fhir_template.html", patient_record=patient_record)
+
+# @app.route("/hl7/patient_summary/download_cda", methods=["GET"])
+# def get_hl7_patient_summary_cda(patient_identifier):
+#     """Generate a CDA document from the FHIR patient record and return it as a file"""
+
+#     patient_record = create_sample_patient_record()
+#     fhir_to_cda(fhir_data=patient_record, patient_identifier=patient_identifier)
+
+#     directory = os.path.join(app.root_path, "static/out")
+#     file_name = f"{patient_identifier}_ps_sample_cda.xml"
+
+#     return send_from_directory(directory, file_name, as_attachment=True)
+
+
+@app.route("/hl7/patient_summary/fhir/select", methods=["GET"])
+def fhir_patient_list():
+    """Get a list of patients from the HAPI server and display them to the user"""
+
+    client = SyncFHIRClient("http://hapi.fhir.org/baseR4")
+    patients = client.resources("Patient").limit(500).fetch()
+
+    patient_list = []
+    for patient in patients:
+        patient_list.append(
+            {
+                "id": patient.id,
+            }
+        )
+        print(patient_list)
+
+    return render_template("fhir_patient_list.html", patients=patient_list)
+
+
+@app.route("/hl7/patient_summary/fhir/patient", methods=["POST"])
+def fhir_patient_summary():
+    """Get the patient id from the user selection and display the patient summary"""
+
+    patient_id = request.form.get("patient_id")
+
+    if patient_id:
+        client = SyncFHIRClient("http://hapi.fhir.org/baseR4")
+        patient = client.resources("Patient").get(id=patient_id)
+
+        # Convert FHIR resources to JSON
+        patient_json = patient.serialize()
+        patient_info = []
+        for key, value in patient_json.items():
+            patient_info.append({"key": key, "value": value})
+
+        return render_template("fhir_patient_summary.html", patient=patient_info)
+
+    return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
